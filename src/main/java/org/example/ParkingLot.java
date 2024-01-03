3603c0768b85e5653e427b049aff2b04ef8d8328
Darkdriller <swaindhruv28@gmail.com>
2024-01-03 16:43:35 +0530

    Added the ability to Park Car (UC1)

---
+++ src/main/java/org/example/ParkingLot.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author Dhruv
+ * @project ParkingLot
+ */
+
+public class ParkingLot {
+    public boolean parkCarForFlight() {
+        System.out.println("Vehicle Parked");
+        return true;
+    }
+}
---
+++ src/test/java/org/example/ParkingLotTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * @author Dhruv
+ * @project ParkingLot
+ */
+import org.junit.Test;
+import static junit.framework.Assert.assertTrue;
+
+public class ParkingLotTest {
+    @Test
+    public void parkCarForFlight_ShouldReturnTrue_ForEveryDriverJourney() {
+        ParkingLot parkingLot = new ParkingLot();
+        boolean isParked = parkingLot.parkCarForFlight();
+        assertTrue(isParked);
+    }
+}
